28ef42bb6237d7ac093a5ce922763ea18ab1b764
Arun Tiwari <itiwariarun438@gmail.com>
2025-03-02 19:33:32 +0530

    feat: pwa

--- next.config.js
+++ next.config.js
@@ -1,5 +1,12 @@
 /** @type {import('next').NextConfig} */
+const withPWA = require("next-pwa");
+
+const pwa = withPWA({
+  dest: "public", // Stores service worker and PWA assets in the public folder
+  disable: process.env.NODE_ENV === "development", // Disable PWA in development
+});
 const nextConfig = {
+  ...pwa,
   reactStrictMode: true,
   env: {
     OPENAI_API_KEY: process.env.OPENAI_API_KEY,
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -14,6 +14,13 @@ class MyDocument extends Document {
       <Html lang="en">
         <Head>
           <title>Code Convertor</title>
+          <link rel="manifest" href="/manifest.json" />
+          <link rel="icon" href="/favicon.svg" />
+          <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+          {/* Apple Touch Icon */}
+          <link rel="apple-touch-icon" href="/favicon.svg" />
+          {/* Theme color for mobile browsers */}
+          <meta name="theme-color" content="#ffffff" />
         </Head>
         <body>
           <Main />
